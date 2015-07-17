285e22f189726bd00bf500b511db3caa361c6c50
Sebastien Barre <sebastien.barre@gmail.com>
2015-07-16 20:17:00 -0400

    chore(build): fix jasmine runner script to run with Babel

--- spec/support/jasmine-runner.js
+++ spec/support/jasmine-runner.js
@@ -1,7 +1,17 @@
-var Jasmine = require('jasmine');
-var SpecReporter = require('jasmine-spec-reporter');
-var noop = function() {};
+'use strict';
 
+var iojs         = require('is-iojs');
+var Jasmine      = require('jasmine');
+var SpecReporter = require('jasmine-spec-reporter');
+
+if (!iojs) {
+  var pjson = require('../../package.json');
+  require('babel/register')({
+    ignore: new RegExp(pjson.name + '/node_modules')
+  });
+}
+
+var noop = function() {};
 var jrunner = new Jasmine();
 jrunner.configureDefaultReporter({print: noop}); // remove default reporters
 jasmine.getEnv().addReporter(new SpecReporter());
